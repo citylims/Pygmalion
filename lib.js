907aac39dce840ccf466b1dd7bd0097da13caa37
Ausin Formica <austin.formica@gmail.com>
2015-04-01 16:43:13 -0400

    remove doodle

--- lib.js
+++ lib.js
@@ -45,46 +45,13 @@ $(document).ready(function() {
     ctx.stroke();
   }
 
-  // threeGreen(80, 100);
-  // threeGreen(110, 160);
-  // threeGreen(140, 160);
-  // threeGreen(170, 200);
-  // threeGreen(200, 160);
-  // threeGreen(230, 160);
-  // threeGreen(290, 200);
+  threeGreen(80, 100);
+  threeGreen(110, 160);
+  threeGreen(140, 160);
+  threeGreen(170, 200);
+  threeGreen(200, 160);
+  threeGreen(230, 160);
+  threeGreen(290, 200);
   threeBlack(300, 300);
 
 });
-
-
-
-
-// ctx.fillStyle = "#CD2322"
-//     ctx.fillRect(90, 85, 100, 100);
-//     ctx.strokeStyle = "#CD2322"
-//     ctx.strokeRect(50, 50, 100, 100);
-//
-//     ctx.lineWidth = 10;
-//     ctx.beginPath();
-//     ctx.moveTo(100, 100);
-//     ctx.lineTo(150, 350);
-//     ctx.lineTo(350, 350);
-//     ctx.closePath();
-//     ctx.stroke();
-//
-//     ctx.lineWidth = 20;
-//     ctx.strokeStyle = "#CD2322"
-//     ctx.strokeRect(300, 360, 100, 100);
-//
-//     ctx.beginPath();
-//     ctx.fillStyle = "#55ACEE"
-//     ctx.arc(300, 100, 50, 0, Math.PI*2, false);
-//     ctx.closePath();
-//     ctx.fill();
-//
-//     ctx.lineWidth = 8;
-//     ctx.beginPath();
-//     ctx.strokeStyle = "#55ACEE"
-//     ctx.moveTo(50, 150);
-//     ctx.quadraticCurveTo(250, 50, 450, 150);
-//     ctx.stroke();
